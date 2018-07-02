2bc0f14eb0d65dac3bc8db0964b6fe3628229ddb
Luan Nico <luanpotter27@gmail.com>
2018-07-02 10:47:53 -0300

    Implementation.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,97 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const fetchPackageName = async (context: vscode.ExtensionContext) => {
+    const files: vscode.Uri[] = await vscode.workspace.findFiles('pubspec.yaml');
+    if (files.length != 1) {
+        vscode.window.showErrorMessage(`Expected to find a single pubspec.yaml file, ${files.length} found.`);
+        return null;
+    }
+    const file : vscode.TextDocument = await vscode.workspace.openTextDocument(files[0]);
+    const fileName = file.fileName.replace(/\/pubspec\.yaml$/, '');
+    const possibleNameLines = file.getText().split('\n').filter(line => line.match(/^\s*name:/));
+    if (possibleNameLines.length != 1) {
+        vscode.window.showErrorMessage(`Expected to find a single line starting with 'name:' on pubspec.yaml file, ${possibleNameLines.length} found.`);
+        return null;
+    }
+    const nameLine = possibleNameLines[0];
+    const regex = /^\s*name:\s*(.*)$/.exec(nameLine);
+    if (!regex) {
+        vscode.window.showErrorMessage(`Expected line 'name:' on pubspec.yaml to match regex, but it didn't (line: ${nameLine}).`);
+        return null;
+    }
+    return {
+        projectRoot: fileName,
+        projectName: regex[1],
+    };
+};
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "dart-import" is now active!');
+export const relativize = (filePath : String, importPath : String) => {
+    const pathSplit = (path : String) => path.length === 0 ? [] : path.split('/');
+    const fileBits = pathSplit(filePath);
+    const importBits = pathSplit(importPath);
+    let dotdotAmount = 0, startIdx;
+    for (startIdx = 0; startIdx < fileBits.length; startIdx++) {
+        if (fileBits[startIdx] === importBits[startIdx]) {
+           continue; 
+        }
+        dotdotAmount = fileBits.length - startIdx;
+        break;
+    }
+    const relativeBits = new Array(dotdotAmount).fill('..').concat(importBits.slice(startIdx));
+    return relativeBits.join('/');
+};
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+export async function activate(context: vscode.ExtensionContext) {
+    const packageInfo = await fetchPackageName(context);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const cmd = vscode.commands.registerCommand('dart-import.fix', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        if (!packageInfo) {
+            vscode.window.showErrorMessage('Failed to initialize extension. Is this a valid Dart/Flutter project?');
+            return;
+        }
+
+    const currentPath = editor.document.fileName.replace(/\/[^\/]*.dart$/, '');
+        const libFolder = packageInfo.projectRoot + '/lib/';
+        if (!currentPath.startsWith(libFolder)) {
+            vscode.window.showErrorMessage('Current file is not on project root or not on lib folder? File must be on $root/lib.');
+            return;
+        }
+        const relativePath = currentPath.substring(libFolder.length);
+        let count = 0;
+        for (let currentLine = 0;; currentLine++) {
+            const line : vscode.TextLine = editor.document.lineAt(currentLine);
+            if (line.text.trim().length === 0) {
+                continue;
+            }
+            const content = line.text.trim();
+            if (!content.startsWith('import ')) {
+                break;
+            }
+            const regex = new RegExp(`^\\s*import\\s*(['"])package:${packageInfo.projectName}/([^'"]*)['"]\\s*;\\s*$`);
+            const exec = regex.exec(content);
+            if (exec) {
+                const quote = exec[1];
+                const importPath = exec[2];
+                const relativeImport = relativize(relativePath, importPath);
+                const content = `import ${quote}${relativeImport}${quote};`;
+                await editor.edit((builder) => {
+                    const start = new vscode.Position(currentLine, 0);
+                    const end = new vscode.Position(currentLine, line.text.length);
+                    builder.replace(new vscode.Range(start, end), content);
+                });
+                count++;
+            }
+        }
+
+        vscode.commands.executeCommand('editor.action.organizeImports');
+        vscode.window.showInformationMessage((count === 0 ? 'No lines changed.' : `${count} imports fixed.`) + ' All imports sorted.');
     });
-
-    context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+    context.subscriptions.push(cmd);
 }
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,32 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import { relativize } from '../extension';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+suite('#relativize test', function () {
+    test('Root file path', function() {
+        const filePath = '';
+        const importPath = 'foo/bar.dart';
+        const result = relativize(filePath, importPath);
+        assert.equal('foo/bar.dart', result);
+    });
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+    test('Shared root', function() {
+        const filePath = 'foo';
+        const importPath = 'foo/bar.dart';
+        const result = relativize(filePath, importPath);
+        assert.equal('bar.dart', result);
+    });
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test('One up', function() {
+        const filePath = 'foo';
+        const importPath = 'bar.dart';
+        const result = relativize(filePath, importPath);
+        assert.equal('../bar.dart', result);
+    });
+
+    test('Some up', function() {
+        const filePath = 'foo/bar/john/carl';
+        const importPath = 'foo/bar/sam/tom.dart';
+        const result = relativize(filePath, importPath);
+        assert.equal('../../sam/tom.dart', result);
     });
 });
